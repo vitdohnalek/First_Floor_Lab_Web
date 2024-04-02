e18ca442c41316eef8ca7ac72454f96de41a424e
Vít Dohnálek <vitdohnalek@gmail.com>
2024-04-02 23:09:06 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+// HAMBURGER NAV
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navMenu = document.querySelector('nav ul');
+
+    menuToggle.addEventListener('click', function() {
+        navMenu.classList.toggle('active');
+    });
+});
+
 //RESEARCH PAGE SLIDESHOW
 // Intersection Observer setup
 const observer = new IntersectionObserver((entries) => {
